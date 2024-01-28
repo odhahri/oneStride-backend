28da503748b6f107e776e970cd4f0bdb44ee7702
Oussama Dhahri <oussamadhahri78@gmail.com>
2024-01-29 00:09:25 +0100

    feat: prepering urls for article - prepering article crud

---
+++ src/article/controllers/article_crud.js
@@ -0,0 +1,17 @@
+const getAllArticles = async (req, res) => {};
+
+const getArticleById = async (req, res) => {};
+
+const createArticle = async (req, res) => {};
+
+const updateArticle = async (req, res) => {};
+
+const deleteArticle = async (req, res) => {};
+
+module.exports = {
+  getAllArticles,
+  getArticleById,
+  createArticle,
+  updateArticle,
+  deleteArticle,
+};
--- src/article/urls.js
+++ src/article/urls.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const articleController = require("./controllers/article_crud");
 
-// Define article routes
-router.get("/", (req, res) => {
-  res.send("Article Home Page");
-});
+// Get all articles
+router.get("/get-all-articles", articleController.getAllArticles);
 
-router.get("/details", (req, res) => {
-  res.send("Article Details Page");
-});
+// Get article by ID
+router.get("/get-article-by-id/:id", articleController.getArticleById);
 
-// Add more routes as needed
+// Create a new article
+router.post("/create-article", articleController.createArticle);
+
+// Update an article by ID
+router.put("/update-article-by-id/:id", articleController.updateArticle);
+
+// Delete an article by ID
+router.delete("/delete-article-by-id/:id", articleController.deleteArticle);
 
 module.exports = router;
